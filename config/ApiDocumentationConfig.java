53b4e61f0582c16002c03ecedc56a470cf050139
Tiago <tiagofleischauer@hotmail.com>
2023-10-25 11:56:01 -0300

    fornecendo dados

--- config/ApiDocumentationConfig.java
+++ config/ApiDocumentationConfig.java
@@ -5,14 +5,12 @@ public class ApiDocumentationConfig {
     public OpenAPI apiDocConfig() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Nome da API")
-                        .description("Descrição da API")
-                        .version("0.0.1") //você sabe como funciona uma versão?
+                        .title("Restaurante IFSUL")
+                        .description("Aplicação de Restaurantes")
+                        .version("0.0.1")
                         .contact(new Contact()
-                                .name("Nome do DEV")
-                                .email("email@do.dev")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("Algum link externo")
-                        .url("https:/wiki...."));
+                                .name("Tiago Fleischauer")
+                                .email("tiagofleischauer@hotmail.com")))
+                .externalDocs(new ExternalDocumentation()));
     }
 }
