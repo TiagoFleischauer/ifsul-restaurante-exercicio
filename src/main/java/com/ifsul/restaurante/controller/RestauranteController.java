85f8ba9a2ddac9c8ea932330d3d6ef34021b51cc
Tiago <tiagofleischauer@hotmail.com>
2023-10-25 12:12:40 -0300

    corrigido codigo service para controller

--- src/main/java/com/ifsul/restaurante/controller/RestauranteController.java
+++ src/main/java/com/ifsul/restaurante/controller/RestauranteController.java
@@ -2,6 +2,12 @@ package com.ifsul.restaurante.controller;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,11 +31,23 @@ public class RestauranteController {
     @Autowired
     private RestauranteService restauranteService;
 
+    @Operation(summary = "Retorna uma lista de restaurantes")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna um array com os restaurantes encontrados",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Restaurante.class))) })
+    })
     @GetMapping
     public List<Restaurante> listarRestaurantes() {
         return restauranteService.listarRestaurantes();
     }
 
+    @Operation(summary = "Busca restaurante por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna o restaurante com o ID fornecido",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Restaurante.class))) })
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Restaurante> buscarRestaurantePorId(@PathVariable Long id) {
         Restaurante restaurante = restauranteService.buscarRestaurantePorId(id);
@@ -40,12 +58,24 @@ public class RestauranteController {
         }
     }
 
+    @Operation(summary = "Cria um restaurante")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cria um novo restaurante",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Restaurante.class))) })
+    })
     @PostMapping
     public ResponseEntity<Restaurante> criarRestaurante(@RequestBody Restaurante restaurante) {
         Restaurante novoRestaurante = restauranteService.criarRestaurante(restaurante);
         return ResponseEntity.status(HttpStatus.CREATED).body(novoRestaurante);
     }
 
+    @Operation(summary = "Atualiza um restaurante")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Atualiza o restaurante com o ID fornecido",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Restaurante.class))) })
+    })
     @PutMapping("/{id}")
     public ResponseEntity<Restaurante> atualizarRestaurante(@PathVariable Long id, @RequestBody Restaurante restaurante) {
         Restaurante restauranteExistente = restauranteService.buscarRestaurantePorId(id);
@@ -57,6 +87,12 @@ public class RestauranteController {
         }
     }
 
+    @Operation(summary = "Deleta um resturante")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Deleta um restaurante com o ID fornecido",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Restaurante.class))) })
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarRestaurante(@PathVariable Long id) {
         Restaurante restauranteExistente = restauranteService.buscarRestaurantePorId(id);
@@ -67,19 +103,37 @@ public class RestauranteController {
             return ResponseEntity.notFound().build();
         }
     }
-    
+
+    @Operation(summary = "Busca restaurantes por nome")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna uma lista de restaurantes com o nome fornecido",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Restaurante.class))) })
+    })
     @GetMapping("/nome/{nome}")
     public List<Restaurante> buscarRestaurantesPorNome(@PathVariable String nome) {
         List<Restaurante> restaurantes = restauranteService.buscarRestaurantesPorNome(nome);
         return restaurantes;
     }
 
+    @Operation(summary = "Busca restaurantes por endereço")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna uma lista de restaurantes com o endereço fornecido",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Restaurante.class))) })
+    })
     @GetMapping("/endereco/{endereco}")
     public List<Restaurante> buscarRestaurantesPorEndereco(@PathVariable String endereco) {
         List<Restaurante> restaurantes = restauranteService.buscarRestaurantesPorEndereco(endereco);
         return restaurantes;
     }
 
+    @Operation(summary = "Busca restaurantes por cozinha")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna uma lista de restaurantes com o tipo de cozinha fornecido",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Restaurante.class))) })
+    })
     @GetMapping("/cozinha/{cozinha}")
     public List<Restaurante> buscarRestaurantesPorTipoCozinha(@PathVariable String cozinha) {
         List<Restaurante> restaurantes = restauranteService.buscarRestaurantesPorTipoCozinha(cozinha);
--- src/main/java/com/ifsul/restaurante/service/RestauranteService.java
+++ src/main/java/com/ifsul/restaurante/service/RestauranteService.java
@@ -2,101 +2,44 @@ package com.ifsul.restaurante.service;
 
 import java.util.List;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ifsul.restaurante.model.Restaurante;
 import com.ifsul.restaurante.repository.RestauranteRepository;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 public class RestauranteService {
     @Autowired
     private RestauranteRepository restauranteRepository;
 
-
-    @Operation(summary = "Retorna uma lista de restaurantes")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Retorna um array com os restaurantes encontrados",
-                    content = { @Content(mediaType = "application/json",
-                            array = @ArraySchema(schema = @Schema(implementation = Restaurante.class))) })
-    })
-    @GetMapping
     public List<Restaurante> listarRestaurantes() {
         return restauranteRepository.findAll();
     }
 
-    @Operation(summary = "Cria um restaurante")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Cria um novo restaurante",
-                    content = { @Content(mediaType = "application/json",
-                            array = @ArraySchema(schema = @Schema(implementation = Restaurante.class))) })
-    })
-    @GetMapping
     public Restaurante criarRestaurante(Restaurante restaurante) {
         return restauranteRepository.save(restaurante);
     }
 
-    @Operation(summary = "Busca um restaurante por ID")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Retorna o restaurante com o ID fornecido",
-                    content = { @Content(mediaType = "application/json",
-                            array = @ArraySchema(schema = @Schema(implementation = Restaurante.class))) })
-    })
-    @GetMapping
     public Restaurante buscarRestaurantePorId(Long id) {
         return restauranteRepository.findById(id).orElse(null);
     }
 
-    @Operation(summary = "Deleta um resturante")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Deleta um restaurante com o ID fornecido",
-                    content = { @Content(mediaType = "application/json",
-                            array = @ArraySchema(schema = @Schema(implementation = Restaurante.class))) })
-    })
-    @GetMapping
     public void deletarRestaurante(Long id) {
         restauranteRepository.deleteById(id);
     }
 
-    @Operation(summary = "Busca um restaurante por nome")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Retorna o restaurante com o nome fornecido",
-                    content = { @Content(mediaType = "application/json",
-                            array = @ArraySchema(schema = @Schema(implementation = Restaurante.class))) })
-    })
-    @GetMapping
     public List<Restaurante> buscarRestaurantesPorNome(String nome) {
         return restauranteRepository.findByNomeContaining(nome);
     }
 
-    @Operation(summary = "Busca um restaurante por endereço")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Retorna o restaurante com o endereço fornecido",
-                    content = { @Content(mediaType = "application/json",
-                            array = @ArraySchema(schema = @Schema(implementation = Restaurante.class))) })
-    })
-    @GetMapping
     public List<Restaurante> buscarRestaurantesPorEndereco(String endereco) {
         return restauranteRepository.findByEnderecoContaining(endereco);
     }
 
-    @Operation(summary = "Busca um restaurante por tipo de cozinha")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Retorna o restaurante com o tipo de cozinha fornecido",
-                    content = { @Content(mediaType = "application/json",
-                            array = @ArraySchema(schema = @Schema(implementation = Restaurante.class))) })
-    })
-    @GetMapping
     public List<Restaurante> buscarRestaurantesPorTipoCozinha(String tipoCozinha) {
         return restauranteRepository.findByTipoCozinhaContaining(tipoCozinha);
     }
-    
-    
+
+
 }
